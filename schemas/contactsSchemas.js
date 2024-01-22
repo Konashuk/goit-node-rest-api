bc911db39877f22b3383d1b9a6eb8b995973a721
Konashuk <konan4ukv@gmail.com>
2024-01-22 14:48:26 +0200

    add validate for post

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -10,4 +10,10 @@ export const updateContactSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string().email(),
   phone: Joi.string(),
-});
+}).custom((value, helpers) => {
+  // Перевірка, чи є хоча б одне поле у значенні
+  if (!(value.name || value.email || value.phone)) {
+    return helpers.error("any.required");
+  }
+  return value;
+}, "custom validation");
